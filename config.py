6eec08a863752a9ee3a575c352d3fbac9409f61d
Johannes Kaufmann <Kaufmann.johannes00@gmail.com>
2024-05-12 20:54:53 +0200

    added config validation

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ class Config:
         self.data_config = self.load_config(os.path.join(base_path, data_config_path))
         self.config = {}
         self.merge_configs()
+        try:
+            self._validate_config()
+        except Exception as e:
+            print(f'Error validating config: {e}')
+            raise e
 
     def load_config(self, config_path: str):
         """Load a YAML configuration file and return a dictionary."""
@@ -39,14 +44,66 @@ class Config:
             'foreground_objects': {k: os.path.join(self.data_config.get('root_path', ''), v) for k, v in self.data_config.get('foreground_objects', {}).items()}
         }     
 
+    def _validate_config(self):
+        self._validate_paths()
+        self._validate_images()
+        self._validate_transformations()
+        self._validate_object_counts()
+        
+    def _validate_transformations(self):
+        for label in self.config['transformations'].keys():
+            if not label in self.config["foreground_objects"].keys():
+                raise ValueError(f'No path defined for label {label} in foreground_objects')
+    
+    def _validate_paths(self):
+        if not self.config['root_path']:
+            raise ValueError('Root path is not set')
+        self._validate_path(self.config['root_path'])
+        self._validate_path(self.config['background_folder'])
+        for label, path in self.config['foreground_objects'].items():
+            self._validate_path(path)
+    
+    def _validate_images(self):
+        self._validate_image(self.config['background_folder'])
+        for label, path in self.config['foreground_objects'].items():
+            self._validate_image(path)
+    def _validate_image(self, path: str):
+        images = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.png') or f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.JPG')]
+        print(path, len(images))
+        if not images:
+            raise ValueError(f'No images found in {path}')
+    
+    def _parse_label(self, label: str) -> List[str]:
+        if label == "all":
+            return list(self.config['foreground_objects'].keys())
+        return label.replace(" ", "").split(',')
+    
+    def _parse_transformation(self, label, transformations: dict, transforms: dict) -> Transformation:
+        labels = self._parse_label(label)
+        for l in labels:
+            if l not in transforms.keys():
+                transforms[l] = []
+            for transformation in transformations:
+                transforms[l].append(create_transformation(**transformation))
+    
     def _parse_transformations(self, transformations: dict) -> List[Transformation]:
         transforms = {}
         for label in transformations.keys():
-            if label not in transforms.keys():
-                transforms[label] = []
-            for transformation in transformations[label]:
-                transforms[label].append(create_transformation(**transformation))
-            return transforms
+            self._parse_transformation(label, transformations[label], transforms)    
+        return transforms
+        
+    def _validate_path(self, path):
+        if not os.path.exists(path):
+            raise FileNotFoundError(f'Path {path} does not exist')
+    
+    def _validate_object_counts(self):
+        for label in self.config["foreground_objects"].keys():
+            if not label in self.config["object_counts"].keys():
+                raise ValueError(f'No object count defined for label {label} in object_counts')
+            if not isinstance(self.config["object_counts"][label], int):
+                raise ValueError(f'Object count for label {label} is not an integer')
+            if self.config["object_counts"][label] < 0:
+                raise ValueError(f'Object count for label {label} is negative')
         
     def save_config(self, config_path):
         with open(config_path, 'w') as stream:
